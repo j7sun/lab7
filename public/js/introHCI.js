8b1cc6f6774cf9a806c71f7f73a49da55da6378a
Jialun Sun <j7sun@ucsd.edu>
2021-02-18 17:25:43 -0800

    lab7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,12 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	$(".likeBtn").click(clickLikeBtn);
 	// your code here
+}
+
+function clickLikeBtn(){
+	e.preventDefault();
+	ga('create', 'UA-190150423-1', 'auto');
+	ga('send','event','like','click');
 }
